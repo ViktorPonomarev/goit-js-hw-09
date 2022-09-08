9568645716de93814867f30d7948364a3cb8b431
ViktorPonomarev <viktorponomarev15.02.89@gmail.com>
2022-09-08 17:54:09 +0300

    лрлощшо

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -4,20 +4,50 @@ import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
 
 const refs = {
-    btnStart: document.querySelector('button[data-start]'),
+  btnStart: document.querySelector('button[data-start]'),
+  clockface: document.querySelector('#datetime-picker'),
+};
 
+/*Запускаем таймер с помощью
+"Адской копипасты"
+ */
+class Timer {
+  constructor({onTick}) {
+    this.intervalId = null;
+    this.isActive = false;
+    this.onTick = onTick;
+
+    this.init();
+  }
+
+  init() {
+    const time = this.convertMs(0);
+    this.onTick(time);
+  }
+
+    start() {
+    if (this.isActive) {
+      return;
 }
 
+    const startTime = Date.now();
+    this.isActive = true;
 
+    this.intervalId = setInterval(() => {
+      const currentTime = Date.now();
+      const deltaTime = startTime - currentTime;
+      const time = this.convertMs(deltaTime);
+      
+      this.onTick(time);
+}, 1000);
+  }
 
-
-
-/*Адская копипаста с библиотеки!
+  /*Адская копипаста с библиотеки!
 Для подсчета значений используй готовую функцию convertMs, 
 где ms - разница между конечной и текущей датой в миллисекундах.
 Показывая четыре цифры: дни, часы, минуты и секунды в формате xx:xx:xx:xx.
  */
-function convertMs(ms) {
+convertMs(ms) {
   // Number of milliseconds per unit of time
   const second = 1000;
   const minute = second * 60;
@@ -25,25 +55,55 @@ function convertMs(ms) {
   const day = hour * 24;
 
   // Remaining days
-  const days = this.addLeadingZero(Math.floor(ms / day));
+  const days = addLeadingZero(Math.floor(ms / day));
   // Remaining hours
-  const hours = this.addLeadingZero(Math.floor((ms % day) / hour));
+  const hours = addLeadingZero(Math.floor((ms % day) / hour));
   // Remaining minutes
-  const minutes = this.addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+  const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
   // Remaining seconds
-  const seconds = this.addLeadingZero(Math.floor((((ms % day) % hour) % minute) / second));
+  const seconds = addLeadingZero(Math.floor((((ms % day) % hour) % minute) / second));
 
   return { days, hours, minutes, seconds };
 }
 
-console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
-console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
-console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
+// console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
+// console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
+// console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
 
 /* Форматирование времени!
 Принимает число, приводит к строке и добавляет 
 в начало 0 если число меньше 2-х знаков.
  */
-function addLeadingZero(value) {
+addLeadingZero(value) {
     return String(value).padStart(2, '0');
+  }
+  
 }
+
+const timer = new Timer({
+  onTick: updateClockfase,
+});
+
+refs.btnStart.addEventListener('click', timer.start.bind(timer));
+
+
+
+/*Принимает время в милисекундах
+Вичесляет сколько в них вмещает дней, часов, минут, секунд
+ */
+function updateClockfase({ days, hours, minutes, seconds }) {
+  refs.clockface.textContent = `${days}:${hours}:${minutes}:${seconds}`;
+}
+
+/*Библиотека flatpickr Дата и время.
+ */
+// {
+//     enableTime: true,
+//     dateFormat: "Y-m-d H:i",
+// }
+
+
+
+
+
+
