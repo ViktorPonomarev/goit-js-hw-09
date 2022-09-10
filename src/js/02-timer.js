052cf93f840ac6808e848551a0e43b29bb72eb8f
ViktorPonomarev <viktorponomarev15.02.89@gmail.com>
2022-09-10 16:15:53 +0300

    арце

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -7,18 +7,143 @@ import Notiflix from 'notiflix';
 
 
 // Достаём елементы
-const refs = {
-  btnStart: document.querySelector('button[data-start]'),
-  clockface: document.querySelector('#datetime-picker'),
-  days: document.querySelector('[data-days]'),
-  hours: document.querySelector('[data-hours]'),
-  minutes: document.querySelector('[data-minutes]'),
-  seconds: document.querySelector('[data-seconds]'),
+const selector = document.querySelector('#datetime-picker');
 
-  timer: document.querySelector('.timer'),
-  field: document.querySelector('.field'),
+const timer = document.querySelector('.timer');
+
+const field = Array.from(document.querySelectorAll('.field'));
+
+const value = Array.from(document.querySelectorAll('.value'));
+
+const label = Array.from(document.querySelectorAll('.label'));
+
+const btnStart = document.querySelector('button[data-start]');
+
+const days = document.querySelector('[data-days]');
+
+const hours = document.querySelector('[data-hours]');
+
+const minutes = document.querySelector('[data-minutes]');
+
+const seconds = document.querySelector('[data-seconds]');
+
+// Работаем с кнопкой 
+// Оформление таймера стилями
+btnStart.disabled = true;
+timer.style.display = 'flex';
+timer.style.marginTop = '10px';
+
+field.forEach(e => {
+  e.style.marginRight = '30px';
+});
+
+value.forEach(e => {
+  e.style.fontSize = '30px';
+  e.style.fontFamily = 'Impact,Charcoal,sans-serif';
+  e.style.fontStyle = 'italic';
+});
+
+label.forEach(e => {
+  e.style.fontSize = '30px';
+  e.style.fontFamily = 'Impact,Charcoal,sans-serif';
+  e.style.fontStyle = 'italic';
+});
+
+// Опции библиотеки flatpickr
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    let deltaTime = selectedDates[0].getTime() - currentDate;
+
+    if (deltaTime <= 0) {
+      Notiflix.Notify.warning('Please choose a date in the future');
+    } else {
+      btnStart.disabled = false;
+     
+      btnStart.addEventListener('click', () => {
+
+        const timer = setInterval(() => {
+          
+          const newTime = Date.now();
+          this.isActive = true;
+          const newDelta = selectedDates[0].getTime() - newTime;
+          const newTimeComp = convertMs(newDelta);
+          seconds.textContent = addLeadingZero(newTimeComp.seconds);
+          minutes.textContent = addLeadingZero(newTimeComp.minutes);
+          hours.textContent = addLeadingZero(newTimeComp.hours);
+          days.textContent = addLeadingZero(newTimeComp.days);
+          console.log(newDelta);
+          if (newDelta <= 1000) {
+            clearInterval(timer);
+
+            
+          }
+        }, 1000);
+      });
+    }
+  },
 
 };
+
+flatpickr(selector, options);
+
+const currentDate = Date.now();
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const refs = {
+//   btnStart: document.querySelector('button[data-start]'),
+//   clockface: document.querySelector('#datetime-picker'),
+//   days: document.querySelector('[data-days]'),
+//   hours: document.querySelector('[data-hours]'),
+//   minutes: document.querySelector('[data-minutes]'),
+//   seconds: document.querySelector('[data-seconds]'),
+
+//   timer: document.querySelector('.timer'),
+//   field: document.querySelector('.field'),
+
+// };
 
 
 
