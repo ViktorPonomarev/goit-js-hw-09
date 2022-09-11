55c02795cced262ecb7d983616e33b01e94e9442
ViktorPonomarev <viktorponomarev15.02.89@gmail.com>
2022-09-12 00:15:35 +0300

    Зделал 9 домашку

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -3,35 +3,68 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 // Достаём елементы
 const refs = {
   form: document.querySelector('.form'),
-  dalay: document.querySelector('[name=delay]'),
+  delay: document.querySelector('[name=delay]'),
   step: document.querySelector('[name=step]'),
   amount: document.querySelector('[name=amount]'),
   button: document.querySelector('button'),
   
 }
 
-let evtDalay = Number(refs.dalay.value);
-let evtStep = Number(refs.dalay.value);
-let evtAmount = Number(refs.dalay.value);
+refs.form.addEventListener('submit', onFormSubmit);
 
-// refs.button.addEventListener('click', e => {
-//   for (let i = 0; i < strongs.length; i += 1) {
-//     strongs[i].style.cssText = `color: yellow;`;
-//   }
 
+
+/*Дополни код функции createPromise так, чтобы она возвращала 
+один промис, который выполянется или отклоняется через delay 
+времени. Значением промиса должен быть объект, в котором будут 
+свойства position и delay со значениями одноименных параметров. 
+Используй начальный код функции для выбора того, что нужно сделать 
+с промисом - выполнить или отклонить.
+ */
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((fulfill, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+     
+      if (shouldResolve) {
+        fulfill({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    });
+  });
 }
 
-// createPromise(2, 1500)
-//   .then(({ position, delay }) => {
-//     console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-//   })
-//   .catch(({ position, delay }) => {
-//     console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-//   });
+ 
+
+function onFormSubmit(e) {
+  e.preventDefault();
+
+ let evtDelay = Number(refs.delay.value);
+let evtStep = Number(refs.step.value);
+let evtAmount = Number(refs.amount.value);
+
+  // Через фор вычесляем значения
+  for (let i = 1; i <= evtAmount; i++) {
+
+    /*Код функции createPromise так, чтобы она возвращала один промис,
+     который выполянется или отклоняется через delay времени.
+    */
+    createPromise(i, evtDelay)
+      .then(({ position, delay }) => {
+        setTimeout(() => {
+          Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+        }, delay);
+    
+      })
+    .catch(({ position, delay }) => {
+      setTimeout(() => { 
+      Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+    }, delay);
+    
+  evtDelay += evtStep});
+  }
+}
+      
+
+
